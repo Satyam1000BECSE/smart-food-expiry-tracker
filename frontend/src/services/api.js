7121a359b964c1d5e6a141118fe7f88aed163188
Satyam1000BECSE <128610133+Satyam1000BECSE@users.noreply.github.com>
2026-02-25 20:40:15 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://smart-food-expiry-tracker.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
