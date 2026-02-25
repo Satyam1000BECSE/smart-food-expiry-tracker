7cbd9822a3722d536a8f5173560bcae62d596709
Satyam1000BECSE <128610133+Satyam1000BECSE@users.noreply.github.com>
2026-02-25 19:43:32 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}  # 🔥 Important for Render
+)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
